e42f7522d626392296664408503348f2b177d817
Fabricio Anciães <>
2017-04-30 18:05:57 -0300

    Lógica da dashboard completa

--- scripts/dashboard.js
+++ scripts/dashboard.js
@@ -1,30 +1,10 @@
-document.addEventListener('DOMContentLoaded', function() {
-    elements = {
-        "pauta": {
-            "classes":"section.pauta",
-            "selector":document.querySelector("section.pauta"),
-            "buttons":document.querySelectorAll("section.pauta .behave-radio"),
-        },
-
-        "casters": {
-            "classes":"section.casters",
-            "selector":document.querySelector("section.casters"),
-        },
-    }
-
-    data = {
-        "pauta":[],
-        "pautaActiveIndex":0,
-        "casters":[],
-    }
-
-    const forEach = function (array, callback, scope) {
+const forEach = function (array, callback, scope) {
         for (let i = 0; i < array.length; i++) {
             callback.call(scope, i, array[i]);
         }
     };
 
-    function getValues(panel, targetArray) {
+function getValues(panel) {
         const myNodeList = document.querySelectorAll(panel.classes+ ' .value');
         let array = [];
 
@@ -33,8 +13,31 @@ document.addEventListener('DOMContentLoaded', function() {
         });
 
         return array;
+    } 
+
+document.addEventListener('DOMContentLoaded', function() {
+    elements = {
+        "pauta": {
+            "classes":"section.pauta",
+            "selector":document.querySelector("section.pauta"),
+            "buttons":document.querySelectorAll("section.pauta .behave-radio"),
+            "update":document.querySelector("section.pauta .update"),
+        },
+
+        "casters": {
+            "classes":"section.casters",
+            "selector":document.querySelector("section.casters"),
+            "update":document.querySelector("section.casters .update")
+        },
     }
-    
+
+    data = {
+        "pauta":[],
+        "pautaActiveIndex":0,
+        "casters":[],
+        "castersCount":0,
+    }
+
     for(let i=0;i < elements.pauta.buttons.length; i++){
         elements.pauta.buttons[i].addEventListener("click", function(e) {
             for(let i=0;i < elements.pauta.buttons.length; i++){
@@ -63,5 +66,25 @@ document.addEventListener('DOMContentLoaded', function() {
         button.classList.remove("is-danger");
         button.classList.add("is-success");
     }
+
+    elements.pauta.update.addEventListener("click", function() {
+        data.pauta = getValues(elements.pauta);
+
+        console.log(data.pauta);
+        console.log(data.pautaActiveIndex);
+    });
+
+    elements.casters.update.addEventListener("click", function(){
+        data.casters = getValues(elements.casters);
+        var count = 0;
+        for(let i = 0; i < data.casters.length; i++) {
+            if(data.casters[i] != "") {
+                count++;
+            }
+        }
+        data.castersCount = count;
+        console.log(data.casters);
+        console.log(data.castersCount);
+    })
 
 });
